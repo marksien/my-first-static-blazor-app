af6bf581fe8e6fe069aee801048869942457d787
marksien <k196xj15@hotmail.com>
2024-07-18 14:39:46 +0800

    加入Fluent UI

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Microsoft.Fast.Components.FluentUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddFluentUIComponents();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
